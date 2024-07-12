c675b664b02c6cff559902e0b3bdb69c95c3168c
prabuddha chunchekar <prabuddhachunchekar@gmail.com>
2024-07-12 20:56:11 +0530

    shopify

--- shopify/seller/views.py
+++ shopify/seller/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from seller.models import Product
+import os
 def dashboard(request):
     data={}
     if(request.user.is_authenticated):
@@ -37,20 +38,20 @@ def delete_product(request, product_id):
 def update_product(request, product_id):
     data={}
     added_products=Product.objects.filter(id=product_id)
-    print(added_products)
-    print(added_products[0])
     data['product']=added_products[0]
 
-    # if(request.method=="POST"):
-    #     name=request.POST['name']
-    #     category=request.POST['category']
-    #     description=request.POST['description']
-    #     price=request.POST['price']
-    #     quantity=request.POST['quantity']
-    #     is_available=request.POST.get('is_available') and ('is_available' in request.POST)
-    #     image = request.FILES.get('image')
+    if(request.method=="POST"):
+        name=request.POST['name']
+        category=request.POST['category']
+        description=request.POST['description']
+        price=request.POST['price']
+        quantity=request.POST['quantity']
+        is_available=request.POST.get('is_available') and ('is_available' in request.POST)
+        image = request.FILES.get('image')
 
-    #     updated_product = Product.objects.update(name=name,price=price, category=category,description=description,quantity=quantity, is_active=is_available,image=image)
 
-        # return redirect("/dashboard")
+        # updated_product = Product.objects.update(name=name,price=price, category=category,description=description,quantity=quantity, is_active=is_available, image=myimage)
+
+
+        return redirect("/dashboard")
     return render(request, "seller/update_product.html", context=data)
